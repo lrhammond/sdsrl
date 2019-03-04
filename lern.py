c4077f057f109bb13e458b0e2af239aded6a97f4
Lewis Hammond <lewis@lewishammond.com>
2019-03-04 19:01:34 +0000

    Restructure files

--- lern.py
+++ lern.py
@@ -8,9 +8,108 @@ import util
 from scipy import optimize as opt
 import numpy as np
 import blox
+from copy import deepcopy
+from random import choice
 
 
-# Learns schemas for a particular object attribute given data X and y
+# Learning procedure
+def hyperMax(mode, numEpisodes, numSteps, numSamples, epsilon)
+
+    # Set up game according to mode and return description of initial state
+    environment = inta.setup(mode,test=True)
+    initState = inta.observeState(mode, environment)
+    rewards = [0 for ep in range(numEpisodes)]
+
+    # Intialise model and Q-function
+    M = blox.Model(mode, initState)
+    M.obsActions = [["UP", "LEFT", "DOWN", "RIGHT", "nothing"],[[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1],[0,0,0,0]]]
+    M.updateDicts()
+    # Q = QFunction(mode)
+
+    # Learn model and Q-function
+    for i in range(numEpisodes):
+        print("===========")
+        print("Episode " + str(i))
+        # Set up model for new episode
+        environment = inta.setup(mode, test=True)
+        initState = inta.observeState(mode, environment)
+        M.initialise(mode, initState)
+        ended = False
+        current_reward = 0
+        for j in range(numSteps):
+            # Check if the game has ended
+            if ended:
+                rewards[i] = current_reward
+                print("*********")
+                print("Game Over")
+                print("Score: " + str(current_reward))
+                print("*********")
+                break
+            else:
+                print("-------")
+                print("Step " + str(j))
+                # Take action in the game and observe reward using RMAX
+                action = rmax(M, Q, epsilon)
+                print("Action: " + action)
+                [reward, ended] = inta.performAction(M, mode, environment, action)
+                current_reward += reward
+                # If the game has ended we only update the action and reward, as the state doesn't matter
+                if not ended:
+                    state = [inta.observeState(mode, environment), action, reward]
+                else:
+                    state[1] = action
+                    state[2] = reward
+                # Update model, data, and schemas
+                M.prev = M.getModelState()
+                M.oldMap = deepcopy(M.objMap)
+                M.updateModel(mode, state)
+                M.curr = M.getModelState()
+                M.updateData()
+                M.learn()
+                # Update Q-function using HYPE
+                for i in range(numSamples):
+                    state = M.curr
+                    hype(M, numEpisodes, i, state, Q)
+
+    print("Rewards:")
+    print rewards
+
+    print("Schemas:")
+    for i in M.schemas:
+        for j in i.keys():
+            for k in i[j]:
+                k.display()
+
+    print("Evidence:")
+    for i in M.evidence:
+        for j in i.keys():
+            for k in i[j]:
+                print k
+
+    print("Remaining:")
+    for i in M.XY:
+        for j in i.keys():
+            for k in i[j]:
+                print k
+
+    return
+
+
+# Chooses and returns action using RMAX framework with M and Q
+def rmax(M, Q, epsilon):
+
+    # TODO
+    return choice(M.obsActions[0][:4])
+
+
+# Updates Q function using abstracted trajectory samples from M
+def hype(M, numEpisodes, i, state, Q):
+
+    # TODO
+    return
+
+
+# Learns schemas for a particular object attribute given data X and y using linear programming
 def learnSchemas(model, xYes, xNo, schemas, R=0.1, L=LIMIT):
     # # If all the cases are positive there are no constraints for learning schemas so we do not try
     # if len(xNo) == 0:
--- test.py
+++ test.py
@@ -1,100 +1,19 @@
-# TEST
++# TEST
 # Script for running tests of other functions
 
-from copy import deepcopy
-from random import choice
-import inta
-import blox
 import lern
-import util
+import veri
 import main
 
 # Runs learning and verification procedures
-def run(mode, numEpisodes, numSteps):
+def run(mode, numEpisodes, numSteps, numSamples, epsilon):
 
-    # Set up game according to mode and return description of initial state
-    environment = inta.setup(mode,test=True)
-    initState = inta.observeState(mode, environment)
-    rewards = [0 for ep in range(numEpisodes)]
-
-    # Intialise model and Q-function
-    M = blox.Model(mode, initState)
-    M.obsActions = [["UP", "LEFT", "DOWN", "RIGHT", "nothing"],[[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1],[0,0,0,0]]]
-    M.updateDicts()
-    # Q = QFunction(mode)
-
-    # Learn model and Q-function
-    for i in range(numEpisodes):
-        print("===========")
-        print("Episode " + str(i))
-        # Set up model for new episode
-        environment = inta.setup(mode, test=True)
-        initState = inta.observeState(mode, environment)
-        M.initialise(mode, initState)
-        ended = False
-        current_reward = 0
-        for j in range(numSteps):
-            # Check if the game has ended
-            if ended:
-                rewards[i] = current_reward
-                print("*********")
-                print("Game Over")
-                print("Score: " + str(current_reward))
-                print("*********")
-                break
-            else:
-                print("-------")
-                print("Step " + str(j))
-                # Take action in the game
-                action = choice(M.obsActions[0][:4])
-                print("Action: " + action)
-                # action = "DOWN"
-                [reward, ended] = inta.performAction(M, mode, environment, action)
-                current_reward += reward
-                # If the game has ended we only update the action and reward, as the state doesn't matter
-                if not ended:
-                    state = [inta.observeState(mode, environment), action, reward]
-                else:
-                    state[1] = action
-                    state[2] = reward
-                # Update model, data, and schemas
-                M.prev = M.getModelState()
-
-                M.oldMap = deepcopy(M.objMap)
-
-                M.updateModel(mode, state)
-
-                M.curr = M.getModelState()
-
-                M.updateData()
-
-                M.learn()
-
-                # Update Q-function using model
-                # Q.update(M)
-
-    print("Rewards:")
-    print rewards
-
-    print("Schemas:")
-    for i in M.schemas:
-        for j in i.keys():
-            for k in i[j]:
-                k.display()
-
-    print("Evidence:")
-    for i in M.evidence:
-        for j in i.keys():
-            for k in i[j]:
-                print k
-
-    print("Remaining:")
-    for i in M.XY:
-        for j in i.keys():
-            for k in i[j]:
-                print k
+    # Learn model, Q-function, and policies
+    lern.hyperMax(mode, numEpisodes, numSteps, numSamples, epsilon)
 
     # Verify properties of model, Q-function, or resulting policies
     # TODO
 
-run("vgdl",5,50)
+    return
+
+run("vgdl",5, 50, 10)
